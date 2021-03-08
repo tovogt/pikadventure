b1989a3625c3de7e62c9678ab5022935480ced6b
Patryk Kubiczek <patryk.kubiczek@pik-potsdam.de>
2021-03-08 17:18:30 +0100

    Revert "Added zoom tiles"
    This reverts commit 44bb450f67983e82ef61046cfeb45dc96989e866.

--- extra_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.02.15" name="extra_tiles" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="zoom.png" width="32" height="32"/>
-</tileset>
